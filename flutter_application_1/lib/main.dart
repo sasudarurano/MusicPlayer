995aabb0a4729c260195a58a1058d0ec58cb3377
urano <darrenlowell_2226240033@mhs.mdp.ac.id>
2024-11-11 00:10:31 +0700

    update main and build gradle

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,12 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return  MaterialApp(
+      title: 'Music Player App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      
+
     );
+
   }
+    
 }
